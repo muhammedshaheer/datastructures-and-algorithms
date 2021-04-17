509d0fb89d1bb4dda571b2696b823f6487b15d9c
Muhammed Shaheer <muhammed.shaheer@litmus7.com>
2021-04-17 12:54:07 +0530

    added stack implementation

---
+++ src/datastructuresfirst/stack/Stack.java
@@ -0,0 +1,65 @@
+package datastructuresfirst.stack;
+
+public class Stack {
+    private final int[] arr;
+    private int top;
+    private final int capacity;
+
+    public Stack(int size) {
+        this.arr = new int[size];
+        this.top = -1;
+        this.capacity = size;
+    }
+
+    public void push(int x) {
+        if (isFull()) {
+            System.out.println("Overflow\nProgram Terminated\n");
+            System.exit(1);
+        }
+
+        System.out.println("Inserting " + x);
+        arr[++top] = x;
+    }
+
+    public int pop() {
+        if (isEmpty()) {
+            System.out.println("Stack Empty");
+            System.exit(1);
+        }
+        return arr[top--];
+    }
+
+    public int size() {
+        return top + 1;
+    }
+
+    public Boolean isEmpty() {
+        return top == -1;
+    }
+
+    public Boolean isFull() {
+        return top == capacity - 1;
+    }
+
+    public void printStack() {
+        for (int i = 0; i < size(); i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack stack = new Stack(5);
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+
+        System.out.println("Popping " + stack.pop());
+        System.out.println("Popping " + stack.pop());
+        System.out.println("Popping " + stack.pop());
+
+        System.out.println("\nAfter popping out");
+        stack.printStack();
+    }
+}
